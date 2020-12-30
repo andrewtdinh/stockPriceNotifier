b7b000ba46338bf34b096f77d6309026b3db1ae4
Andrew Dinh <andrewd@benchling.com>
2020-12-30 00:31:47 -0800

    Re-add the server config file

---
+++ server/config.js
@@ -0,0 +1,11 @@
+const config = {
+  user: '<DATABASE_USER>',
+  password: '<DATABASE_PASSWORD>',
+  host: '<DATABASE_HOST>',
+  port: '<DATABASE_PORT>',
+  database: '<DATABASE_NAME>',
+  ssl: '<IS_SSL>',
+  apiHost: 'https://www.alphavantage.co/',
+};
+
+module.exports = config;

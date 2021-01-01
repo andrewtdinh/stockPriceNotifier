4c37fd6ea1bb6195ccf0c4748e4c9752a5ed13a9
Andrew Dinh <andrewd@benchling.com>
2020-12-31 18:04:02 -0800

    Modify config file

--- server/config.js
+++ server/config.js
@@ -1,11 +1,23 @@
 const config = {
-  user: '<DATABASE_USER>',
-  password: '<DATABASE_PASSWORD>',
-  host: '<DATABASE_HOST>',
-  port: '<DATABASE_PORT>',
-  database: '<DATABASE_NAME>',
-  ssl: '<IS_SSL>',
-  apiHost: 'https://www.alphavantage.co/',
+  databaseOptions: {
+    user: '<DATABASE_USER>',
+    password: '<DATABASE_PASSWORD>',
+    host: '<DATABASE_HOST>',
+    port: '<DATABASE_PORT>',
+    database: '<DATABASE_NAME>',
+    ssl: true,
+  },
+  apiHostOptions: {
+    host: 'https://www.alphavantage.co/',
+    key: '<API_KEY>',
+    timeSeriesFunction: 'TIME_SERIES_INTRADAY',
+    interval: '5min'
+  },
+  graphqlURL: '<GRAPHQL_URL>'
 };
 
-module.exports = config;
+const getConfig = (key) => {
+  return config[key];
+};
+
+module.exports = getConfig;
